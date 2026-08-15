ba3a7d67e242400b41be3e392d371f9ca7ae1afc
Dustin Almon <ddalmon43@gmail.com>
2026-08-15 01:06:12 -0700

    Moved logging functions into logger module

--- cyber_terminal.py
+++ cyber_terminal.py
@@ -9,6 +9,7 @@ import socket
 from datetime import datetime
 from urllib import response
 from network_tools import grab_banner, ping_target, resolve_target, run_scan
+from logger import write_log, show_logs, clear_logs
 
 
 # =========================
@@ -22,27 +23,6 @@ RESET = "\033[0m"
 # =========================
 # LOGGING FUNCTIONS
 # =========================
-
-def write_log(message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    with open("cyber_log.txt", "a") as file:
-        file.write(f"[{timestamp}] {message}\n")
-
-
-def show_logs():
-    try:
-        with open("cyber_log.txt", "r") as file:
-            print(file.read())
-    except FileNotFoundError:
-        print("No logs found yet.")
-
-
-def clear_logs():
-    open("cyber_log.txt", "w").close()
-    print("Logs cleared.")
-    write_log("[SYSTEM] Logs were cleared")
-
 
 # =========================
 # NETWORK FUNCTIONS
--- logger.py
+++ logger.py
@@ -6,3 +6,16 @@ def write_log(message):
 
     with open("cyber_log.txt", "a") as file:
         file.write(f"[{timestamp}] {message}\n")
+
+def show_logs():
+    try:
+        with open("cyber_log.txt", "r") as file:
+            print(file.read())
+    except FileNotFoundError:
+        print("No logs found yet.")
+
+
+def clear_logs():
+    open("cyber_log.txt", "w").close()
+    print("Logs cleared.")
+    write_log("[SYSTEM] Logs were cleared")
